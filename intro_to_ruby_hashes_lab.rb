6f396b8a541b539d41d138254fcbc8ed7970ff50
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-23 03:19:47 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,15 +19,8 @@ def monopoly_with_second_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	   :rent_in_dollars =>
-	   {
-	   #   :one_piece_owned => 25,
-	   #   :two_pieces_owned => 50,
-	   #   :three_pieces_owned => 100,
-	   #   :four_pieces_owned => 200
-	   },
-	    :names => {}
-	   # {
+	     :names =>
+	     {
 	   #   :reading_railroad =>
 	   #   {
 	   #     "mortgage_value" => "$100"
@@ -44,7 +37,15 @@ def monopoly_with_second_tier
 	   #   {
 	   #     "mortgage_value" => "$800"
 	   #   }
-	    #}
+	      },
+	   :rent_in_dollars =>
+	   {
+	   #   :one_piece_owned => 25,
+	   #   :two_pieces_owned => 50,
+	   #   :three_pieces_owned => 100,
+	   #   :four_pieces_owned => 200
+	   }
+	   
 	  }
 	}
 	
